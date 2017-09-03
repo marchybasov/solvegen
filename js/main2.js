5b2f626e336a1749457befc1493b28825bde3d54
marchybasov <m_archybasov@mail.ua>
2017-09-03 23:03:29 +0300

    last commit

--- js/main2.js
+++ js/main2.js
@@ -1,31 +1,6 @@
 ;(function ($) {
     "use strict";
 
-    // $(function () {
-    //     $('.main-top-product-slider').slick({
-    //         slidesToShow: 5,
-    //         slidesToScroll: 1,
-    //         dots: true,
-    //         autoplay: true,
-    //         autoplaySpeed: 2000,
-    //         responsive: [
-    //
-    //             {
-    //                 breakpoint: 1000,
-    //                 settings: {
-    //                     arrows: false
-    //                 }
-    //             },
-    //             {
-    //                 breakpoint: 480,
-    //                 settings: {
-    //                     dots: false,
-    //                     arrows: true
-    //                 }
-    //             }
-    //         ]
-    //     });
-    // });
     $(document).ready(function () {
         $(document).on("click",".main-middle__switcher-sizeBtn--item", function (e) {
                 $(".main-middle__switcher-sizeBtn--item").removeClass("active");
@@ -44,8 +19,68 @@
                 arrows: true,
                 slickRemove: 2
 
+
             });
         });
+
+    $(function (){
+
+        $('.main-bottom-story--slider').slick({
+            slidesToShow: 5,
+            slidesToScrool: 1,
+            infinite: true,
+            // centermode: true,
+            dots: true,
+
+            responsive: [
+                {
+                    breakpoint: 1920,
+                    settings: {
+                        slidesToShow: 4,
+
+
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 1380,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 1,
+                        variableWidth: true,
+                        centerMode: true
+                    }
+                },
+                {
+                    breakpoint: 980,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+
+                    }
+                },
+                {
+                    breakpoint: 710,
+                    settings: {
+
+                        infinite: true,
+                        centerPadding: '20%',
+                        slidesToShow: 1,
+                        variableWidth: false
+                    }
+                },
+                {
+                    breakpoint: 640,
+                    settings: {
+                        centerMode: true,
+                        infinite: true,
+                        centerPadding: '27%',
+                        slidesToShow: 1,
+                        variableWidth: false
+                    }
+                }]
+        });
+    });
 
 
 })(jQuery);
