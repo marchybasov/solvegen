6a3180d72205ccda3866fa9923e899c3898e0ff3
marchybasov <m_archybasov@mail.ua>
2017-09-07 12:44:51 +0300

    trying to make perfectpixel(4)

--- js/main2.js
+++ js/main2.js
@@ -2,28 +2,28 @@
     "use strict";
 
     $(document).ready(function () {
-        $(document).on("click",".main-middle__switcher-sizeBtn--item", function (e) {
-                $(".main-middle__switcher-sizeBtn--item").removeClass("active");
-                $(e.currentTarget).addClass('active');
+        $(document).on("click", ".main-middle__switcher-sizeBtn--item", function (e) {
+            $(".main-middle__switcher-sizeBtn--item").removeClass("active");
+            $(e.currentTarget).addClass('active');
         });
     });
 
 
-    $(function (){
+    $(function () {
 
-            $('.ing-slider2').slick({
-                slidesToShow: 1,
-                slidesToScrool: 1,
-                infinite: true,
-                centermode: true,
-                arrows: true,
-                slickRemove: 2
+        $('.ing-slider2').slick({
+            slidesToShow: 2,
+            slidesToScrool: 1,
+            infinite: true,
+            centermode: true,
+            arrows: true,
+            slickRemove: 1
 
 
-            });
         });
+    });
 
-    $(function (){
+    $(function () {
 
         $('.main-bottom-story--slider').slick({
             slidesToShow: 5,
@@ -33,52 +33,52 @@
             dots: true,
 
             responsive: [
-                {
-                    breakpoint: 1920,
-                    settings: {
-                        slidesToShow: 4,
-
-
-                        slidesToScroll: 1
-                    }
-                },
+                // {
+                //     breakpoint: 1920,
+                //     settings: {
+                //         slidesToShow: 2,
+                //
+                //
+                //         slidesToScroll: 1
+                //     }
+                // },
                 {
                     breakpoint: 1380,
                     settings: {
                         slidesToShow: 2,
-                        slidesToScroll: 1,
-                        variableWidth: true,
-                        centerMode: true
-                    }
-                },
-                {
-                    breakpoint: 980,
-                    settings: {
-                        slidesToShow: 1,
                         slidesToScroll: 1
-
+                        // centerMode: true
                     }
-                },
-                {
-                    breakpoint: 710,
-                    settings: {
-
-                        infinite: true,
-                        centerPadding: '20%',
-                        slidesToShow: 1,
-                        variableWidth: false
-                    }
-                },
-                {
-                    breakpoint: 640,
-                    settings: {
-                        centerMode: true,
-                        infinite: true,
-                        centerPadding: '27%',
-                        slidesToShow: 1,
-                        variableWidth: false
-                    }
-                }]
+                }
+                // {
+                //     breakpoint: 900,
+                //     settings: {
+                //         slidesToShow: 1,
+                //         slidesToScroll: 1
+                //
+                //     }
+                // },
+                // {
+                //     breakpoint: 710,
+                //     settings: {
+                //
+                //         infinite: true,
+                //         centerPadding: '20%',
+                //         slidesToShow: 1,
+                //         variableWidth: false
+                //     }
+                // },
+                // {
+                //     breakpoint: 640,
+                //     settings: {
+                //         centerMode: true,
+                //         infinite: true,
+                //         centerPadding: '27%',
+                //         slidesToShow: 1,
+                //         variableWidth: false
+                //     }
+                // }
+                ]
         });
     });
 
